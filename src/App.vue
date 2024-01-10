b79ad2b1e8fde136a44954bb97869b8b5f7bd0a7
Amely Elena Charris Heins <116146886+amelych@users.noreply.github.com>
2024-01-10 13:31:35 +0100

    primeras pruebas

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Bienvenido a  Vue.js App"/>
+  <PrimerasPruebas mensaje="Primeras pruebas" />
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import PrimerasPruebas from './components/PrimerasPruebas.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    PrimerasPruebas
   }
 }
 </script>
---
+++ src/components/PrimerasPruebas.vue
@@ -0,0 +1,67 @@
+<template>
+    <div class="hello">
+        <h3>{{ mensaje }}</h3>
+        <h4>El resultado de la suma es {{ resultadoSuma }}</h4>
+        <h5>Listado de Items:</h5>
+        <ul>
+            <li v-for="item in listaItems" :key="item.id">
+              {{ item.id }} -- {{ item.nombre }} -- {{ item.apellido }}
+            </li>
+        </ul>
+        <h5>Nombre del cliente: {{ nombreCliente }}</h5>
+        <button @click='incrementar'>Incrementar</button>
+        <h5>Contador: {{ contador }}</h5>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "PrimerasPruebas",
+    props: {
+        mensaje: String,
+        numero1: {
+            type: Number,
+            default: 0
+        },
+        numero2: {
+            type: Number,
+            default: 0
+        },
+        nombre: {
+            type: String,
+            default: "Pepe"
+        },
+        apellidos: {
+            type: String,
+            default: "Perez"
+        }
+    },
+    computed: {
+        nombreCliente(){
+            return this.nombre + ' ' + this.apellidos;
+        }
+    },
+    methods: {
+        incrementar(){
+            this.contador++ 
+        }
+    },
+    data() {
+        return{
+            resultadoSuma: this.numero1 + this.numero2,
+            listaItems: [
+                { id: 1, nombre: 'Juan', apellido: 'Perez' },
+                { id: 2, nombre: 'Susana', apellido: 'Garcia' },
+                { id: 3, nombre: 'Luis', apellido: 'Gomez' },
+                { id: 4, nombre: 'Ana', apellido: 'Gonzalez' },
+                { id: 5, nombre: 'Maria', apellido: 'Lopez' },
+            ],
+            contador: 0
+        };
+    }
+}
+</script>
+
+<style>
+
+</style>
