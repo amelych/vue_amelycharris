0317920fd9e59c02d992de23fb75746867764e08
Amely Elena Charris Heins <116146886+amelych@users.noreply.github.com>
2024-01-15 09:32:01 +0100

    limpio para empezar proyecto

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,19 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Bienvenido a  Vue.js App"/>
-  <PrimerasPruebas mensaje="Primeras pruebas" />
-  <ListaTareas mensajeL="Lista tareas" />
+  <div>
+    <p>pruebas</p>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-import PrimerasPruebas from './components/PrimerasPruebas.vue';
-import ListaTareas from './components/ListaTareas.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
-    PrimerasPruebas,
-    ListaTareas
+
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
