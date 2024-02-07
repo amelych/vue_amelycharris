c035e1f8449d2b89b5d0819f89325bdbd5899494
Amely Elena Charris Heins <116146886+amelych@users.noreply.github.com>
2024-02-07 13:03:24 +0100

    Base de datos funcional

--- src/router/tareas.mjs
+++ src/router/tareas.mjs
@@ -3,14 +3,55 @@ const rutas = express.Router();
 
 import tarea from '../models/tarea.mjs';
 
+rutas.use(express.json());
+
 rutas.get('/', async(req, res) => {
     const tareas = await tarea.find();
     res.json(tareas);
 });
 
-// rutas.post('/', async(req, res) => {
-//     new tarea();
-//     console.log(new tarea());
-// })
+rutas.post('/', async(req, res) => {
+    try {
+        const nuevatarea = new tarea(req.body);
+        await nuevatarea.save();
+        console.log(nuevatarea);
+        res.json({
+            status: 'Tarea guardada'
+        })
+    } catch (error) {
+        console.error('Error al guardar la tarea:', error);
+        res.status(500).json({
+            error: 'Error al guardar la tarea'
+        });
+    }
+});
+
+rutas.put('/:id', async(req, res) => {
+    try {
+        await tarea.findByIdAndUpdate(req.params.id, req.body);
+        res.json({
+            status: 'Tarea actualizada'
+        })
+    } catch (error) {
+        console.error('Error al actualizar la tarea:', error);
+        res.status(500).json({
+            error: 'Error al actualizar la tarea'
+        });
+    }
+});
+
+rutas.delete('/:id', async(req, res) => {
+    try {
+        await tarea.findByIdAndDelete(req.params.id);
+        res.json({
+            status: 'Tarea eliminada'
+        })
+    } catch (error) {
+        console.error('Error al eliminar la tarea:', error);
+        res.status(500).json({
+            error: 'Error al eliminar la tarea'
+        });
+    }
+});
 
 export default rutas;
