30828b17b8ae3ee8c4db4da75b86d41371a87446
Amely Elena Charris Heins <116146886+amelych@users.noreply.github.com>
2024-02-29 09:41:37 +0100

    botones redireccionan

--- src/components/PaginaInicio.vue
+++ src/components/PaginaInicio.vue
@@ -8,12 +8,15 @@
   <div class="container text-center mt-5">
     <div class="row mx-auto">
       <div class="col">
-        <button type="button" class="btn btn-primary btn-lg m-3 p-5 w-25">
+        <router-link to="/clientes" class="navbar-brand">
+          <button type="button" class="btn btn-primary btn-lg m-3 p-5 w-25">
           Gestión Cliente
+        </button></router-link>
+        <router-link to="/tareas" class="navbar-brand">
+          <button type="button" class="btn btn-primary btn-lg m-3 p-5 w-25">
+          Gestión Tareas
         </button>
-        <button type="button" class="btn btn-primary btn-lg m-3 p-5 w-25">
-          Gestión Articulos
-        </button>
+        </router-link>
       </div>
     </div>
     <div class="row mx-auto">
