fa5771a8fbdbbb00cdd40f839ab24a99c0cd412d
Amely Elena Charris Heins <116146886+amelych@users.noreply.github.com>
2024-03-01 11:21:31 +0100

    actualizado

--- src/main.js
+++ src/main.js
@@ -7,6 +7,8 @@ import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import 'bootstrap/dist/js/bootstrap.min.js'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap-icons/font/bootstrap-icons.css'
+import 'sweetalert2/dist/sweetalert2'
+import '@fortawesome/fontawesome-free'
 
 
 const app = createApp(App);
